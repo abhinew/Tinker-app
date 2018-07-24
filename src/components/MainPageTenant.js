5d6fe45c8c4eb904cb30277d79880cb5ce00ccb8
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-07-24 13:29:00 +0200

    added library and MainPageTenants/Owners

---
+++ src/components/MainPageTenant.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { connect } from 'react-redux'
+
+
+class MainPageTenant extends React.PureComponent {
+    render(){
+      return ( <div>
+          <h1></h1>
+          <div className="image">
+              <img/>
+          </div>
+          </div> 
+          )
+    }
+  }
+
+export default connect(null, {})(MainPageTenant)
+
+        
+    
