1b103c27c7ad23a01e6f3d2f700edb5e7b611da9
abhinew <abhinaya.sv@gmail.com>
2018-07-24 15:29:18 +0200

    added route exact path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Route } from 'react-router';
+import FirstPageComponent from './components/FirstPage.js';
+import  MainPageOwner from './components/MainPageOwner.js';
+import MainPageTenant from './components/MainPageTenant.js';
+
 
 
 class App extends Component {
@@ -10,7 +15,9 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          
+          <Route exact path ='/main' component = {FirstPageComponent} />
+          <Route exact path ='/main/owner' component = {MainPageOwner} />
+          <Route exact path ='/main/tenant' component = {MainPageTenant} />
         </div>
       </Provider>
     );
