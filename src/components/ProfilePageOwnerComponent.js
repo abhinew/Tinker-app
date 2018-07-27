a3c6dbe3b9976a3c13e2f5080df3cb556f647d28
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-07-27 09:45:46 +0200

    added css

--- src/components/ProfilePageOwnerComponent.js
+++ src/components/ProfilePageOwnerComponent.js
@@ -11,14 +11,39 @@ class ProfilePageOwnerComponent extends React.PureComponent {
                       <div className="menu">
                         <div className="topButtons">
                             <Link to={"/owners/"}>Back</Link>
-                            <Link to='/chat'>Logt</Link>
+                            <Link to='/chat'>Logout</Link>
                         </div>
                     </div>
+
                     <div className="personalInfoOwner">
-                    <div className="ownerName">
-                    <h2>{this.props.owner[0].name}sdafda</h2>
+                        <div className="ownerName">
+                            <h2>{this.props.owner[0].name}</h2>
+                            <img src />
+                        </div>
                     </div>
+                    <div className="potentialCandidatesOwner">
+                        <h2>You've got {this.props.owner[1].likeByTenant.length} Potential Candidates!</h2>
                     </div>
+                    <div className="ownerCandidates">
+                        <div className="candidateContainer">
+                            <img className="imgOwnerCandidate" src={this.props.tenant[this.props.owner[1].likeByTenant[1]].url}/>
+                            <ul>
+                                <li><b>Name:</b> {this.props.tenant[this.props.owner[1].likeByTenant[1]].name}</li>
+                                <li><b>Age:</b> {this.props.tenant[this.props.owner[1].likeByTenant[1]].age}</li>
+                                <li><b>Occupation:</b> {this.props.tenant[this.props.owner[1].likeByTenant[1]].occupation}</li>
+                                <li><b>Company:</b> {this.props.tenant[this.props.owner[1].likeByTenant[1]].company}</li>
+                                <li><b>Income per month:</b> {this.props.tenant[this.props.owner[1].likeByTenant[1]].incomePerMonth} €</li>
+                            </ul>
+                         </div>
+                    </div>
+                    <div className="ownerMatches">
+                         <h2>You've got {this.props.owner[1].like.length} matches!</h2>
+                    </div>
+                    <div className="propertyButtons">
+                        <button className="addPropertyButton">Add Property</button>
+                        <button className="addPropertyButton">Adjust Property</button>
+                        <button className="deletePropertyButton">Delete Property</button>
+                    </div>                    
                 </div>
 
         )
