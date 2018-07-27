e535278e9ad74d260690512efe00cc65ee2d35a2
Sjoerd Ligtvoet <sjoerd_ligtvoet@hotmail.com>
2018-07-27 08:54:51 +0200

    yeah

--- src/components/ProfilePageOwnerComponent.js
+++ src/components/ProfilePageOwnerComponent.js
@@ -1,20 +1,35 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default class ProfilePageOwnerComponent extends React.PureComponent {
+class ProfilePageOwnerComponent extends React.PureComponent {
 
     render() {
-        console.log(this.props)
+        console.log(this.props.tenant)
         return (
                 <div>
                       <div className="menu">
                         <div className="topButtons">
                             <Link to={"/owners/"}>Back</Link>
-                            <Link to='/chat'>Logout</Link>
+                            <Link to='/chat'>Logt</Link>
                         </div>
+                    </div>
+                    <div className="personalInfoOwner">
+                    <div className="ownerName">
+                    <h2>{this.props.owner[0].name}sdafda</h2>
+                    </div>
                     </div>
                 </div>
 
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+      tenant: state.tenant, // for the first time this is equal to the initial state defined in ./reducers/newWord
+      owner: state.owner
+    }
+}
+
+export default connect(mapStateToProps) (ProfilePageOwnerComponent) 
